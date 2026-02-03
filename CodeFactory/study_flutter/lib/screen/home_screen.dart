16d02343749e7bd7cb5258cfb2588dfe31a42ade
yonghyeok Choi <0570yh@gmail.com>
2026-02-03 23:31:27 +0900

    #2

---
+++ CodeFactory/study_flutter/lib/screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:study_flutter/const/colors.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: colors.map(
+              (e) => Container(
+                height:50.0 ,
+                width: 50.0,
+                color:e)
+              ).toList(),
+                 ),
+        )
+      )  
+    );
+  }
+}
