00ca435b9daa7b64a95a8a04bced9e23ed2d74d7
yonghyeok Choi <0570yh@gmail.com>
2026-02-06 21:52:18 +0900

    study web view

--- CodeFactory/study_flutter/lib/main.dart
+++ CodeFactory/study_flutter/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:study_flutter/screen/home_screen.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
-      home: HomeScreen()
-    )
+      home: HomeScreen(),
+      ),
   );
 }
+
--- CodeFactory/study_flutter/lib/screen/home_screen.dart
+++ CodeFactory/study_flutter/lib/screen/home_screen.dart
@@ -1,66 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:study_flutter/const/colors.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+
+  final WebViewController controller = WebViewController()
+      ..loadRequest(homeUrl);
+
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ...colors.map((e)=>Container(
-                    width: 50.0,
-                    height:50.0,
-                    color:e
-                    ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ...colors.where((e) => e == Colors.orange)
-                  .map((e)=>Container(
-                    width: 50.0,
-                    height:50.0,
-                    color:e
-                    ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ...colors.map((e)=>Container(
-                    width: 50.0,
-                    height:50.0,
-                    color:e
-                    ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ...colors.where((e) => e == Colors.green)
-                  .map((e)=>Container(
-                    width: 50.0,
-                    height:50.0,
-                    color:e
-                    ))
-                ],
-              ),
-            ],
-          ),
-        )
-      )  
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+      ),
+      body:WebViewWidget(
+        controller:controller,
+      ),
     );
   }
 }
