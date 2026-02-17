7934b09edb691a53868e0f617a9c563c39320e5f
yonghyeok Choi <0570yh@gmail.com>
2026-02-17 20:07:34 +0900

    security: remove api key files and update gitignore

--- CodeFactory/google_map/ios/Runner/AppDelegate.swift
+++ CodeFactory/google_map/ios/Runner/AppDelegate.swift
@@ -8,7 +8,11 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(googleMapsApiKey)
+    
+    // Info.plist에서 값을 읽어옵니다.
+    let key = Bundle.main.object(forInfoDictionaryKey: "GoogleMapsApiKey") as? String
+    GMSServices.provideAPIKey(key ?? "")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
