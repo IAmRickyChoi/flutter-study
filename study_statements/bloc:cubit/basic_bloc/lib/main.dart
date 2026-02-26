ecdda9b88e8407ab779b490fd5a98bad83eb78f7
yonghyeok Choi <0570yh@gmail.com>
2026-02-27 01:07:32 +0900

    cubit basic

---
+++ study_statements/bloc:cubit/basic_bloc/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cubit_load/counter/counter_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: CounterPage()));
+}
