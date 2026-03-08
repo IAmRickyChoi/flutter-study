8e3ed62ff768625a126c0752d3d69413389d0094
yonghyeok Choi <0570yh@gmail.com>
2026-03-08 21:13:39 +0900

    basic provider

---
+++ study_statements/provider/basic_provider/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:basic_provider/counter_model.dart';
+import 'package:basic_provider/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
