19a533c436b89d8c69f8be90bb736bf7e7ef4199
yonghyeok Choi <0570yh@gmail.com>
2026-05-23 20:56:44 +0900

    feat:refreshToken

--- CodeFactory/actual/lib/user/view/login_screen.dart
+++ CodeFactory/actual/lib/user/view/login_screen.dart
@@ -25,12 +25,6 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final dio = Dio();
 
-    //localhost
-    final emulatorIp = '10.0.2.2';
-    final simulatorIp = '127.0.0.1:3000';
-
-    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
-
     return DefaultLayout(
       child: SingleChildScrollView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -104,18 +98,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
 
                 TextButton(
-                  onPressed: () async {
-                    final refreshToken =
-                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTc3ODc2NzQ5MCwiZXhwIjoxNzc4ODUzODkwfQ.fSE_FjphX7v6bj9eoeqmWpuJlp16KGklJVF0j6HVGg0';
-                    final resp = await dio.post(
-                      'http://$ip/auth/token',
-                      options: Options(
-                        headers: {'authorization': 'Bearer $refreshToken'},
-                      ),
-                    );
-
-                    print(resp.data);
-                  },
+                  onPressed: () async {},
                   style: TextButton.styleFrom(foregroundColor: Colors.black),
                   child: Text('회원가입'),
                 ),
